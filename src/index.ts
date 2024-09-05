0797a3fa40f2c2b2173139448b47246dc06b5988
Benjamin Buhot <buhot.benjamin@free.fr>
2024-09-05 19:13:17 +0200

    feat: fetch open data and save to file

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+import { gbfs, StationsInformation } from './velib-types';
+import fs from 'fs'
+import tls from 'tls'
+import path from 'path'
+import cron from 'node-cron'
+
+tls.DEFAULT_MIN_VERSION = 'TLSv1.3'
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+const stationInfoFile = path.join(__dirname, 'station_info.json');
+
+const getOpenDataUrl = async (type: string) => {
+    const gbfsReq = await fetch("https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/gbfs.json")
+    const gbfsData : gbfs = await gbfsReq.json()
+    const gbfsFeeds = gbfsData.data.en.feeds
+    return gbfsFeeds.find(feed => feed.name == type)?.url
+}
+
+const fetchAndSaveStationInfo = async () => {
+    const url = await getOpenDataUrl("station_information")
+    if (url) {
+        const stationInfoReq = await fetch(url)
+        const stationInfoData : StationsInformation = await stationInfoReq.json()
+        fs.writeFileSync(stationInfoFile, JSON.stringify(stationInfoData, null, 2));
+        console.log("fetched data!")
+    }
+    
+} 
+
+app.get('/', (req: Request, res: Response) => {
+    try {
+        const data = fs.readFileSync(stationInfoFile, 'utf-8');
+        res.json(JSON.parse(data));
+    } catch (error) {
+        res.status(500).send('Error reading data file');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is Fire at http://localhost:${port}`);
+});
---
+++ src/velib-types.ts
@@ -0,0 +1,90 @@
+interface gbfs {
+    lastUpdatedOther: number
+    ttl: number
+    data: {
+        en: {
+            feeds: {name: string, url: string}[]
+        }
+    }
+}
+
+interface StationsStatus {
+    lastUpdatedOther: number
+    ttl: number
+    data: {
+        stations: {
+            station_id: number,
+            num_bikes_available: number,
+            numBikesAvailable: number,
+            num_bikes_available_types: [{mechanical: number}, {ebike: number}],
+            num_docks_available: number,
+            numDocksAvailable: number,
+            is_installed: number,
+            is_returning: number,
+            is_renting: number,
+            last_reported: number,
+            stationCode: string
+        }[]
+    }
+}
+
+interface StationsInformation {
+    lastUpdatedOther: number
+    ttl: number
+    data: {
+        stations: {
+            station_id: number,
+            stationCode: string,
+            name: string,
+            lat: number,
+            lon: number,
+            capacity: number,
+            rental_methods?: string[]
+        }[]
+    }
+}
+
+interface StationBikes {
+    station: {
+        gps: gpsCoord
+        stationType: string
+        state: string
+        name: string
+        code: string
+        type: string
+        dueDate: number
+    }
+    nbBike: number
+    nbEbike: number
+    nbFreeDock: number
+    nbFreeEDock: number
+    creditCard: "yes" | "no"
+    nbDock: number
+    nbEDock: number
+    nbBikeOverflow: number
+    kioskState: "yes" | "no"
+    overflow: "yes" | "no"
+    overflowActivation: "yes" | "no"
+    maxBikeOverflow: number
+    densityLevel: number
+    bikes: {
+        bikeBlockCause: string
+        bikeElectric: "yes" | "no"
+        bikeName: string
+        bikeRate: 3
+        bikeStatus: string
+        dockPosition: string
+        lastRateDate: string
+        numberOfRates: number
+        stationId: number
+    }[]
+    nbBikeBlockedToCollect: number
+    nbBikeBlockedToFixInStation: number
+    isAltitude: number
+}
+
+[{"maxBikeOverflow":27,"densityLevel":1,"bikes":[{"bikeBlockCause":"","bikeElectric":"yes","bikeName":"68903","bikeRate":3,"bikeStatus":"disponible","dockPosition":"6","lastRateDate":"2024-09-04T06:35:31Z","numberOfRates":2,"stationId":54000623}],"nbBikeBlockedToCollect":0,"nbBikeBlockedToFixInStation":0,"isAltitude":1}]
+
+type gpsCoord = {latitude: number, longitude: number}
+
+export type {StationsStatus, StationsInformation, StationBikes, gbfs, gpsCoord}
