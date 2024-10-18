604f8b14eae7dd93f2f4e031001509701a1533a1
Benjamin Buhot <buhot.benjamin@free.fr>
2024-10-18 02:04:02 +0200

    feat: query parameters validation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response , Application } from 'express';
+import express, { Express, Request, Response , Application, NextFunction } from 'express';
 import dotenv from 'dotenv';
 import {VelibRes, Station} from './velib-stations'
 import tls from 'tls'
@@ -43,12 +43,66 @@ cron.schedule('0 0 * * *', () => {
 });
 
 app.get('/', (req: Request, res: Response) => {
+    res.json("")
+});
+
+const checkQueryParams = (req: Request, res: Response, next: NextFunction) => {
+    if (!(req.query.startLat && req.query.startLon)) {
+        res.status(400)
+        res.json({error: "Missing GPS coordinates"})
+        return
+    }
+    const startLat = parseFloat(req.query.startLat as string)
+    const startLon = parseFloat(req.query.startLon as string)
+    const latWithinBoundaries = 48.572554 < startLat && startLat < 49.05546
+    const lonWithinBoundaries = 1.898879 < startLon && startLon < 2.662193
+    if (!(latWithinBoundaries && lonWithinBoundaries)) {
+        res.status(400)
+        res.json({error: "GPS coordinates out of boundary"})
+        return
+    }
+
+    const validReqType = ['bike', 'eBike', 'mBike', 'dock'].includes(req.query.reqType as string)
+    if (!(req.query.reqType && validReqType)) {
+        console.log(req.query.reqType)
+        res.status(400)
+        res.json({error: "Invalid request type ('bike' | 'eBike' | 'mBike' | 'dock')"})
+        return
+    }
+    const minRate = parseInt(req.query.minRate as string)
+    const minRateWithinRange = 0 < minRate && minRate < 4
+    if (req.query.minRate && !minRateWithinRange) {
+        res.status(400)
+        res.json({error: "Minimum bike rate out of range (1 to 3 stars)"})
+        return
+    }
+
+    const maxLastRate = parseInt(req.query.maxLastRate as string)
+    const maxLastRateWithinRange = 0 < maxLastRate && maxLastRate < 720
+    if (req.query.maxLastRate && !maxLastRateWithinRange) {
+        res.status(400)
+        res.json({error: "Maximum last bike rating out of range (1 to 720 hours)"})
+        return
+    }
+
+    const maxWalkTime = parseInt(req.query.maxWalkTime as string)
+    const maxWalkTimeWithinRange = 0 < maxWalkTime && maxWalkTime < 120
+    if (req.query.maxWalkTime && !maxWalkTimeWithinRange) {
+        res.status(400)
+        res.json({error: "Maximum station walking time out of range (1 to 120 minutes)"})
+        return
+    }
+
+    next()
+}
+
+app.get('/api', checkQueryParams, (req: Request, res: Response) => {
     const params = {
-        startPos: {latitude: 48.82413881129852, longitude: 2.376952760541059},
-        minRate: 3,
-        maxLastRate: 72,
-        bikeType: "eBike",
-        maxWalkTime: 15
+        startPos: {latitude: parseFloat(req.query.startLat as string), longitude: parseFloat(req.query.startLon as string)},
+        minRate: parseInt(req.query.minRate as string),
+        maxLastRate: parseInt(req.query.maxLastRate as string),
+        bikeType: req.query.reqType as string,
+        maxWalkTime: parseInt(req.query.maxWalkTime as string)
     }
     const velibs = new VelibRes(params)
     velibs.filterStations().then((stations: Station[]) => {
@@ -58,8 +112,7 @@ app.get('/', (req: Request, res: Response) => {
         })
         res.json(result)
     })
-    
-});
+})
 
 app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`);
