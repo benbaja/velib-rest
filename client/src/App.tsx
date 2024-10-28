deecbc2cc7accced074973f420190e82d1f50455
Benjamin Buhot <buhot.benjamin@free.fr>
2024-10-28 16:33:09 +0100

    feat: picker component for request type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, Rating, Title } from '@mantine/core';
 import './App.css'
+import { useState } from 'react';
+import TypePicker from './components/TypePicker';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const typeChoiceHook = useState("bike")
+  const [minRate, setMinRate] = useState(3)
 
   return (
     <MantineProvider>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Title order={1}>Velib REST</Title>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        Trouvez rapidement la meilleure station de Velib proche de vous!
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <TypePicker choiceHook={typeChoiceHook} />
+      <Rating defaultValue={3} size="xl" count={3} value={minRate} onChange={setMinRate} />
+      <p className="footer">
+        API Ref - GitHub
       </p>
     </MantineProvider>
   )
---
+++ client/src/components/TypePicker.tsx
@@ -0,0 +1,47 @@
+
+import { ActionIcon, Divider, Group } from '@mantine/core';
+import { MouseEvent, useState } from 'react';
+import { MdElectricBike, MdLocalParking, MdPedalBike } from 'react-icons/md';
+
+interface typePickerProps {
+    choiceHook: [ string, React.Dispatch<React.SetStateAction<string>> ]
+}
+
+const TypePicker: React.FC<typePickerProps> = ({choiceHook}) => {
+    const [ choice, setChoice ] = choiceHook
+    const [ mBikePicked, setMBikePicked] = useState(true)
+    const [ eBikePicked, setEBikePicked] = useState(true)
+    const [ parkPicked, setParkPicked] = useState(false)
+
+    const handleClick = (e: MouseEvent) => {
+        const btnType = e.currentTarget.id
+        if (btnType.includes("bike")) {
+            setParkPicked(false)
+            if (btnType == "mbike") {
+                !mBikePicked ? setMBikePicked(true) : eBikePicked && setMBikePicked(false)
+                mBikePicked && setChoice("mbike")
+            } else if (btnType == "ebike") {
+                !eBikePicked ? setEBikePicked(true) : mBikePicked && setEBikePicked(false)
+                eBikePicked && setChoice("ebike")
+            }
+            mBikePicked && eBikePicked && setChoice("bike")
+        } else if (btnType == "park") {
+            setMBikePicked(false)
+            setEBikePicked(false)
+            !parkPicked && setParkPicked(true)
+        }
+    }
+
+    return (
+        <>
+            <Group>
+                <ActionIcon id="mbike" variant={mBikePicked ? "filled" : "outline"} color="teal" radius="xl" size="xl" onClick={handleClick} ><MdPedalBike size={28} /></ActionIcon>
+                <ActionIcon id="ebike" variant={eBikePicked ? "filled" : "outline"} color="blue" radius="xl" size="xl" onClick={handleClick}><MdElectricBike size={28} /></ActionIcon>
+                <Divider orientation="vertical" />
+                <ActionIcon id="park" variant={parkPicked ? "filled" : "outline"} color="grape" radius="xl" size="xl" onClick={handleClick}><MdLocalParking size={28} /></ActionIcon>
+            </Group>
+        </>
+    )
+}
+
+export default TypePicker
