51b8d1e461006c51e40875a7524ffd67700ca4d8
Benjamin Buhot <buhot.benjamin@free.fr>
2024-11-05 10:53:28 +0100

    feat: auth

--- server/src/velib-stations.ts
+++ server/src/velib-stations.ts
@@ -11,7 +11,7 @@ const reqInit: RequestInit = {
         "Host": "www.velib-metropole.fr",
         "accept": "*/*",
         "content-type": "application/json",
-//        "authorization": `Basic ${process.env.VELIB_API_TOKEN}`,
+        "authorization": `Basic ${process.env.VELIB_API_TOKEN}`,
         "user-agent": "Velib/1099 CFNetwork/1496.0.7 Darwin/23.5.0",
         "accept-language": "fr-FR,fr;q=0.9",
         "Connection": "keep-alive",
@@ -158,7 +158,7 @@ class VelibRes {
         console.log("fetching all stations")
         try {
             const velibRes = await fetch('https://www.velib-metropole.fr/api/map/details?gpsTopLatitude=49.05546&gpsTopLongitude=2.662193&gpsBotLatitude=48.572554&gpsBotLongitude=1.898879&zoomLevel=1', reqInit)
-            console.log(velibRes.body)
+            console.log(velibRes)
             const allStations = await velibRes.json() as any as StationStatus[]
             return allStations.map((stationStatus) => new Station(stationStatus))
         } catch(err) {
